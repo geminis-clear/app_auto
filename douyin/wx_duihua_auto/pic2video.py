507f8e89be0342aebdb4f656cedf182cbbb074f1
麦扬 <>
2022-09-09 15:07:07 +0800

    commit: first commint

--- douyin/wx_duihua_auto/pic2video.py
+++ douyin/wx_duihua_auto/pic2video.py
@@ -8,20 +8,47 @@
 @desc    : 图片转视频
 """
 import cv2
+from PIL import Image
 import os
+import moviepy.editor as mov
 
-fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-cap_fps = 30
 
-size = (376, 688)
-video = cv2.VideoWriter('result.mp4', fourcc, cap_fps, size)
+def video(name):
+    fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+    cap_fps = 0.5
 
-path = './'
-file_list = os.listdir(path)
-print(file_list)
-for filename in file_list:
-    if 'png' in filename:
-        img = cv2.imread(path + filename)
-        img = cv2.resized(img, size)
-        video.write(img)
-video.release()
+    size = (376, 688)
+    save_path = f'video/{name}.mp4'
+    video = cv2.VideoWriter(save_path, fourcc, cap_fps, size)
+
+    path = f'./imgs/{name}/'
+    file_list = os.listdir(path)
+    print(file_list)
+    for filename in file_list:
+        if 'png' in filename:
+            img = cv2.imread(path + filename)
+            img = cv2.resize(img, size)
+            video.write(img)
+    video.release()
+    set_bg_music(save_path)
+
+
+def set_bg_music(videofile):
+    print(videofile)
+    print('添加背景音乐')
+    # 初始化视频文件对象
+    clip = mov.VideoFileClip(videofile)
+    # # 从某个视频中提取一段背景音乐
+    # audio = mov.AudioFileClip('./source.mp4').subclip(0, 83)
+    # # 将背景音乐写入.mp3文件
+    # audio.write_audiofile('./background.mp3')
+    # 向合成好的无声视频中添加背景音乐
+    audio = mov.AudioFileClip('audio/1.mp3')
+    clip = clip.set_audio(audio)
+    # 保存视频
+    clip.write_videofile(videofile)
+    print('背景音乐添加完成！')
+
+
+# video('rr')
+set_bg_music('video/rr.mp4')
--- douyin/wx_duihua_auto/wx_duihua.py
+++ douyin/wx_duihua_auto/wx_duihua.py
@@ -1,6 +1,8 @@
+import os.path
 import time
 from PIL import Image
 from selenium import webdriver
+from pic2video import video
 
 
 def rand_username_face(browser):
@@ -18,11 +20,11 @@ def rand_username_face(browser):
         face_btn[i].click()
 
 
-def screenshot_iphone(browser):
+def screenshot_iphone(browser, filename):
     js = "document.getElementsByClassName('i-body')[0].scrollTop=10000"
     browser.execute_script(js)
     iphone = browser.find_element_by_id('iphone')
-    iphone.screenshot('rr.png')
+    iphone.screenshot(filename)
     # print(iphone.size)
     # size = iphone.size
     # # browser.find_element_by_id('iphone').screenshot('rr.png')
@@ -33,46 +35,56 @@ def screenshot_iphone(browser):
     bottom = 668
     # right = size['width']
     # bottom = size['height']
-    im = Image.open('rr.png')
+    im = Image.open(filename)
     im_crop = im.crop((left, top, right, bottom))
-    im_crop.save('rrr.png')
+    im_crop.save(filename)
 
 
-def main(browser):
+def main(browser, filepath):
     """
     生成文本对话
     :param browser:
     :return:
     """
-    a_u_contents = browser.find_elements_by_xpath('//textarea[starts-with(@class, "a-u-content")]')
-    user1 = a_u_contents[0]
-    user2 = a_u_contents[1]
-    add_content = browser.find_elements_by_xpath('//div[@class="a-u-dialog"]/a')
-    add1 = add_content[0]
-    add2 = add_content[1]
-    with open('rr', 'r', encoding='utf8') as f:
-        contents = f.read()
-        contents = contents.split('\n')
-        print(contents)
-        for content in contents:
-            time.sleep(1)
-            key, value = content.split(':')
-            if key == '0':
-                print('user1')
-                user1.clear()
-                user1.send_keys(value)
-                add1.click()
-                is_del_duihua(browser)
-                screenshot_iphone(browser)
-            elif key == '1':
-                print('user2')
-                user2.clear()
-                user2.send_keys(value)
-                add2.click()
-                is_del_duihua(browser)
-                screenshot_iphone(browser)
-            else:
-                print('error user !!!')
+    try:
+        a_u_contents = browser.find_elements_by_xpath('//textarea[starts-with(@class, "a-u-content")]')
+        user1 = a_u_contents[0]
+        user2 = a_u_contents[1]
+        add_content = browser.find_elements_by_xpath('//div[@class="a-u-dialog"]/a')
+        add1 = add_content[0]
+        add2 = add_content[1]
+        with open(filepath, 'r', encoding='utf8') as f:
+            contents = f.read()
+            contents = contents.split('\n')
+            print(contents)
+            path_ = filepath.split('/')[-1]
+            dir_path = f'imgs/{path_}'
+            if not os.path.exists(dir_path):
+                os.mkdir(dir_path)
+            for i, content in enumerate(contents):
+                filename = f'{dir_path}/{i}.png'
+                time.sleep(1)
+                key, value = content.split(':')
+                if key == '0':
+                    print('user1')
+                    user1.clear()
+                    user1.send_keys(value)
+                    add1.click()
+                    is_del_duihua(browser)
+                    screenshot_iphone(browser, filename)
+                elif key == '1':
+                    print('user2')
+                    user2.clear()
+                    user2.send_keys(value)
+                    add2.click()
+                    is_del_duihua(browser)
+                    screenshot_iphone(browser, filename)
+                else:
+                    print('error user !!!')
+
+            video(path_)
+    except Exception as e:
+        print(e)
 
 
 def is_del_duihua(browser):
@@ -89,19 +101,25 @@ def is_del_duihua(browser):
 
 
 def _init(browser):
-    top_name = browser.find_element_by_class_name('input-common')
-    top_name.clear()
-    top_name.send_keys('')
-
     nav_tabs = browser.find_elements_by_xpath('//ul[contains(@class, "nav-tabs")]/li')
     show_setting = nav_tabs[0]
     duihua_setting = nav_tabs[1]
-    duihua_setting.click()
 
+    show_setting.click()
+    time.sleep(2)
+    top_name = browser.find_element_by_class_name('input-common')
+    if top_name:
+        top_name.clear()
+        top_name.send_keys('')
+    browser.find_elements_by_name('i-b-nick1')[0].click()
+    time.sleep(1)
+
+    duihua_setting.click()
     # browser.find_element_by_xpath('//input[contains(@class, "clear-dialog")]').click()
     msg_del = browser.find_elements_by_xpath('//a[@class="msg-del"]')
     for i in msg_del:
         i.click()
+    time.sleep(2)
 
     browser.find_element_by_class_name('a-u-dialog-master').click()
 
@@ -109,8 +127,10 @@ def _init(browser):
 browser = webdriver.Chrome()
 browser.get('http://127.0.0.1:8888/default.html')
 browser.maximize_window()
-_init(browser)
 # rand_username_face(browser)
-main(browser)
+path_list = ['duihua/rr', 'duihua/rrr']
+for filepath in path_list:
+    _init(browser)
+    main(browser, filepath)
 time.sleep(3)
 browser.close()
